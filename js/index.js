89b97544cbb5eed962805d9e76cc1032681e201d
maryband <40365394+maryband@users.noreply.github.com>
2018-07-04 15:16:41 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,3 +17,18 @@ function fibonacci(number) {
     
     return fibNext;
 } 
+
+let number = parseFloat(prompt('Enter number of sequence'));
+const result = fibonacci(number);
+document.write('The element with position ' + number + ' in the Fibonacci sequence is ' + result);
+
+function fibonacci(number) {
+
+    let fibArr = [1, 1];
+
+    for (let i = 3; i <= number; i++) {
+        fibArr.push(fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]);
+    }
+
+    return fibArr[number - 1];
+}
